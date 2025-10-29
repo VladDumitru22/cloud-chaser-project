8967f025985bf05eac8a8da8017bbe7d2b4b5aa2
VladDumitru22 <vlad.liviu22@gmail.com>
2025-10-30 01:30:56 +0200

    fix: Implement normalization for email

--- backend/crud.py
+++ backend/crud.py
@@ -6,10 +6,11 @@ pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def create_user(db: Session, user: schemas.UserCreate):
     restrictions.validate_password(user.password)
+    normalized_email = user.email.lower()
     hashed_password = pwd_context.hash(user.password)
     db_user = models.User(
         name=user.name,
-        email=user.email,
+        email=normalized_email,
         password_hash=hashed_password,
         phone_number=user.phone_number,
         address=user.address,
@@ -20,4 +21,5 @@ def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 def get_user_by_email(db: Session, email):
-    return db.query(models.User).filter(models.User.email == email).first()
+    normalized_email = email.lower()
+    return db.query(models.User).filter(models.User.email == normalized_email).first()
